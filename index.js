47dc6bc1bbad5dec47ea49c25b0c6e3f94ad2c4a
Daniel Clements <drclements@liberty.edu>
2022-11-02 13:45:44 -0400

    commit tree 25d6d96dab85153835232b7850aae3bbdde63bc0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (driverArray, driverName) {
+   return driverArray.filter((driver) => {
+        return driver.toLowerCase() === driverName.toLowerCase()
+    })
+}
+findMatching(drivers, "bobby")
+console.log(findMatching(driverArray, "bobby"))
+
+function fuzzyMatch (driverArray, driverName) {
+    return driverArray.filter( function (driver) {
+        return driver.toLowerCase().substring(0, driverName.length) === driverName.toLowerCase();
+
+    })
+}
+
+console.logfuzzyMatch(drivers, "sa")
+
+function matchName(driverArray, driverName) {
+    return driverArray.filter( function (driver) {
+        return driver.name === driverName
+    })
+}
+
